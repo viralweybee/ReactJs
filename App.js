fb74278ebdb6f30dbdeae0565afbc0c1dfc1ef1d
Viral Devmurari <viral@weybee.in>
2024-01-11 18:53:08 +0530

    complete till lecture 3

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-const heading=React.createElement('h1',{id:"heading"},"Hello from React");
-const root=ReactDOM.createRoot(document.getElementById('root'))
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+//React Core - Element
+const heading = React.createElement('h1', { id: "heading" }, "This is Create using react core");
+
+
+//Jsx
+const jsHeading = (<div className="root1">
+    <h1 id="heading1">Namaste React using Jsx</h1>
+</div>)
+
+//Component in React
+//Class-Based Component - Old
+//Function - Based Component -New
+const Heading=()=>{
+    return <h1>Namaste React it is a functional Component</h1>
+};
 
 
 
-
-const parent = React.createElement('div', { id: "parent" },
-    [React.createElement('div', { id: "Child" },
-        [React.createElement('h1', {}, "I am h1 tag"),React.createElement('h1', {}, "I am h1 tag1")]
-    ),
-    React.createElement('div', { id: "Child2" },
-        [React.createElement('h1', {}, "I am h1 tag"),React.createElement('h1', {}, "I am h1 tag1")]
-    )
-]
-);
-
-const para=React.createElement('p',{},"I am paragrapht");
-const ob=ReactDOM.createRoot(document.getElementById('footer'));
-ob.render(para);
-
-root.render(parent)
+root.render(<Heading />);
---
+++ ReadME.md
@@ -0,0 +1,13 @@
+# Namaste React
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in CPP
+- Caching - Faster Build
+- Image Optimization
+- Minification
+- Bundling 
+- Compress your file
+
